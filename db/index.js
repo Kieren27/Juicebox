aeb55947ec005d894656d6a0c1e7acc4098720a6
Kieren Enright <kierenenright27@gmail.com>
2023-05-11 12:55:13 -0700

    Part 2: change rows in (getAllUsers) to destructure user from rows

--- db/index.js
+++ db/index.js
@@ -3,12 +3,12 @@ const { Client } = require('pg');
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 async function getAllUsers() {
-    const { rows } = await client.query(`
+    const { rows: [ user ] } = await client.query(`
         SELECT id, username
         FROM users;
         `);
 
-    return rows;
+    return user;
 }
 
 async function createUser({
