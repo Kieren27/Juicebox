356477def01141fd6e60ea26ac68f4dd673cae88
Kieren Enright <kierenenright27@gmail.com>
2023-05-14 12:17:40 -0700

    Part 3: fixed CREATE TABLE post_tags query, added test to call getPostsByTagName('#happy')

--- db/seed.js
+++ db/seed.js
@@ -7,8 +7,7 @@ const {
   getAllPosts,
   updatePost,
   getUserById,
-  createTags,
-  addTagsToPost
+  getPostsByTagName
 } = require('./index');
 
 async function dropTables() {
@@ -57,8 +56,9 @@ async function createTables() {
         );
 
         CREATE TABLE post_tags (
-          "postId" INTEGER REFERENCES posts(id) UNIQUE,
-          "tagId" INTEGER REFERENCES tags(id) UNIQUE
+          "postId" INTEGER REFERENCES posts(id),
+          "tagId" INTEGER REFERENCES tags(id),
+          UNIQUE ("postId", "tagId")
         );
       `);
 
@@ -141,7 +141,7 @@ async function createInitialPosts() {
 //     console.log("Finished creating tags!");
 //   } catch (error) {
 //     console.log("Error creating tags!");
-//     console.error(error);
+//     throw error;
 //   }
 // }
 
@@ -188,6 +188,16 @@ async function testDB() {
     console.log("Calling getUserById with 1");
     const albert = await getUserById(1);
     console.log("Result:", albert);
+
+    console.log("Calling updatePost on posts[1], only updating tags");
+    const updatePostTagsResult = await updatePost(posts[1].id, {
+      tags: ["#youcandoanything", "#redfish", "#bluefish"]
+    });
+    console.log("Result:", updatePostTagsResult);
+
+    console.log("Calling getPostsByTagName with #happy");
+    const postsWithHappy = await getPostsByTagName("#happy");
+    console.log("Result:", postsWithHappy);
 
     console.log("Finished database tests!");
   } catch (error) {
