60a6fbe3da1365806d5db237d72ca3275dd72529
Kieren Enright <kierenenright27@gmail.com>
2023-05-14 12:56:49 -0700

    changed casing of varchar(255) in createTables query to uppercase

--- db/seed.js
+++ db/seed.js
@@ -35,24 +35,24 @@ async function createTables() {
     await client.query(`
         CREATE TABLE users (
           id SERIAL PRIMARY KEY,
-          username varchar(255) UNIQUE NOT NULL,
-          password varchar(255) NOT NULL,
-          name varchar(255) NOT NULL,
-          location varchar(255) NOT NULL,
+          username VARCHAR(255) UNIQUE NOT NULL,
+          password VARCHAR(255) NOT NULL,
+          name VARCHAR(255) NOT NULL,
+          location VARCHAR(255) NOT NULL,
           active BOOLEAN DEFAULT true
         );
 
         CREATE TABLE posts (
           id SERIAL PRIMARY KEY,
           "authorId" INTEGER REFERENCES users(id) NOT NULL,
-          title varchar(255) NOT NULL,
+          title VARCHAR(255) NOT NULL,
           content TEXT NOT NULL,
           active BOOLEAN DEFAULT true
         );
 
         CREATE TABLE tags (
           id SERIAL PRIMARY KEY,
-          name varchar(255) UNIQUE NOT NULL
+          name VARCHAR(255) UNIQUE NOT NULL
         );
 
         CREATE TABLE post_tags (
