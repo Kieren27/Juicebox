d020f90b8bceb76df5adf9c6a7e99e8089a9918c
Kieren Enright <kierenenright27@gmail.com>
2023-05-14 11:02:25 -0700

    Part 3: add tags to posts

--- db/seed.js
+++ db/seed.js
@@ -7,7 +7,8 @@ const {
   getAllPosts,
   updatePost,
   getUserById,
-  createTags
+  createTags,
+  addTagsToPost
 } = require('./index');
 
 async function dropTables() {
@@ -117,6 +118,29 @@ async function createInitialPosts() {
   }
 }
 
+async function createInitialTags() {
+  try {
+    console.log("Starting to create tags...");
+
+    const [happy, sad, inspo, catman] = await createTags([
+      '#happy',
+      '#worst-day-ever',
+      '#youcandoanything',
+      '#catmandoeverything'
+    ]);
+
+    const [postOne, postTwo, postThree] = await getAllPosts();
+
+    await addTagsToPost(postOne.id, [happy, inspo]);
+    await addTagsToPost(postTwo.id, [sad, inspo]);
+    await addTagsToPost(postThree.id, [happy, catman, inspo]);
+
+    console.log("Finished creating tags!");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 async function rebuildDB() {
   try {
     client.connect();
@@ -125,6 +149,7 @@ async function rebuildDB() {
     await createTables();
     await createInitialUsers();
     await createInitialPosts();
+    await createInitialTags();
   } catch (error) {
     throw error;
   }
