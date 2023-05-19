e5b195b19c551501ae91bcb8d8ed948b841d35bb
Kieren Enright <kierenenright27@gmail.com>
2023-05-18 19:30:17 -0700

    installed morgan

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const PORT = 3000;
 const express = require('express');
 const server = express();
 
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.json());
+
+
 server.use((req, res, next) => {
     console.log("<____Body Logger START____>");
     console.log(req.body);
