27e4e48c1037a23ca4f0b098a5004f74e7a16d22
Kieren Enright <kierenenright27@gmail.com>
2023-05-20 15:40:52 -0700

    add function (getUserByUsername) to db, updated '/login' usersRouter to return errors based on users inputs while logging in

--- api/users.js
+++ api/users.js
@@ -17,9 +17,33 @@ usersRouter.get('/', async (req, res) => {
     });
 });
 
+const { getUserByUsername } = require('../db');
+
 usersRouter.post('/login', async (req, res, next) => {
-    console.log(req.body);
-    res.end();
-})
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        next({
+            name: "MissingCridentialsError",
+            message: "Please supply both a username and password"
+        });
+    }
+
+    try {
+        const user = await getUserByUsername(username)
+
+        if (user && user.password == password) {
+            res.send({ message: "you're logged in!" });
+        } else {
+            next({
+                name: "IncorrectCridentialsError",
+                message: "Username or password is incorrect"
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
 
 module.exports = usersRouter;
--- db/index.js
+++ db/index.js
@@ -99,6 +99,23 @@ async function getUserById(userId) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT 
+        *
+      FROM
+        users
+      WHERE
+        username=$1;
+    `, [username]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 async function createPost({
   authorId,
   title,
@@ -372,6 +389,7 @@ module.exports = {
   updateUser,
   getAllUsers,
   getUserById,
+  getUserByUsername,
   createPost,
   updatePost,
   getAllPosts,
