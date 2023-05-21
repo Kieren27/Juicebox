e5eadcf671e0941fedaf9ba95e912758bfcb5559
Kieren Enright <kierenenright27@gmail.com>
2023-05-20 18:03:34 -0700

    add patch method that enables user to edit only their own posts if they are logged in

--- api/posts.js
+++ api/posts.js
@@ -9,7 +9,12 @@ postsRouter.use((req, res, next) => {
     next();
 });
 
-const { getAllPosts, createPost } = require('../db');
+const { 
+    getAllPosts, 
+    createPost, 
+    updatePost, 
+    getPostById 
+} = require('../db');
 
 postsRouter.get('/', async (req, res) => {
     const posts = await getAllPosts();
@@ -37,6 +42,41 @@ postsRouter.post('/', requireUser, async (req, res, next) => {
         const post = await createPost(postData);
 
         res.send({ post });
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
+
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+    const { postId } = req.params;
+    const { title, content, tags } = req.body;
+
+    const updateFields = {};
+
+    if (tags && tags.length > 0) {
+        updateFields.tags = tags.trim().split(/\s+/);
+    }
+
+    if (title) {
+        updateFields.title = title;
+    }
+
+    if (content) {
+        updateFields.content = content;
+    }
+
+    try {
+        const originalPost = await getPostById(postId);
+
+        if (originalPost.author.id === req.user.id) {
+            const updatedPost = await updatePost(postId, updateFields);
+            res.send({ post: updatedPost })
+        } else {
+            next({
+                name: 'UnauthorizedUserError',
+                message: 'You cannot edit a post that is not yours'
+            })
+        }
     } catch ({ name, message }) {
         next({ name, message });
     }
